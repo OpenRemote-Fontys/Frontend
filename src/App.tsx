033cdecc15e62b50256132786c3619fddcd2812c
Robin van Hoof <robbie@vanhoof-erp.nl>
2024-05-15 13:00:39 +0200

    Added free room layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import MapComponent from './components/map/Map';
 import SuggestionComponent from './components/suggestions/Suggestions';
 import FooterComponent from './components/footer/Footer';
+import EmptyRoomsComponent from './components/emptyRooms/EmptyRooms';
 
 function App() {
 	useEffect(() => {}, []);
@@ -10,6 +11,9 @@ function App() {
 		<div className="app-container">
 			<div className="suggestions-container">
 				<SuggestionComponent />
+			</div>
+			<div className="EmptyRooms-container">
+				<EmptyRoomsComponent />
 			</div>
 			<div className="map-container">
 				<MapComponent />
---
+++ src/components/emptyRooms/EmptyRooms.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const EmptyRoomsComponent = () => {
+return (
+<div className="EmptyRooms-wrapper">
+<div className="EmptyRooms-card card">This is a empty room Container.</div>
+</div>
+);
+};
+
+export default EmptyRoomsComponent;
