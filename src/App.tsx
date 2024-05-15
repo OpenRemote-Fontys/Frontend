516b24fa2aa244976d1cda7c57fac65ee9e54fe4
Robin van Hoof <robbie@vanhoof-erp.nl>
2024-05-15 13:18:45 +0200

    Taken out unused App.tsx library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import MapComponent from './components/map/Map';
 import SuggestionComponent from './components/suggestions/Suggestions';
 import FooterComponent from './components/footer/Footer';
