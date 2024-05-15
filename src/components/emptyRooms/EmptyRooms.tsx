2abbeb9af52c86fe82ad6074e63ea367af122270
Robin van Hoof <robbie@vanhoof-erp.nl>
2024-05-15 13:17:28 +0200

    Taken out unused libraries

--- src/components/emptyRooms/EmptyRooms.tsx
+++ src/components/emptyRooms/EmptyRooms.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import './styles.css';
 
 const EmptyRoomsComponent = () => {
-return (
-<div className="EmptyRooms-wrapper">
-<div className="EmptyRooms-card card">This is a empty room Container.</div>
-</div>
-);
+    return (
+        <div className="EmptyRooms-wrapper">
+            <div className="EmptyRooms-card card">This is a empty room Container.</div>
+        </div>
+    );
 };
 
 export default EmptyRoomsComponent;
