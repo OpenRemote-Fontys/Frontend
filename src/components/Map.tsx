82e61699307a3ca069c0f2f725b4195adcef7417
Thijnmens <thijnsmulders04@gmail.com>
2024-06-25 13:05:07 +0200

    Add a legenda for the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,6 +9,12 @@ const MapComponent = () => {
 		<>
 			<div className="w-full h-inherit pt-2.5 overflow-hidden">
 				<div className="h-full w-full p-6">
+					<div className="flex flex-row">
+						<h1 className="font-thin px-2">Legenda: </h1>
+						<span className="bg-red-600 px-5">Loud</span>
+						<span className="bg-yellow-500 px-5">Noisy</span>
+						<span className="bg-green-600 px-5">Silent</span>
+					</div>
 					<MapCanvas onUpdate={setLastUpdate} />
 				</div>
 			</div>
